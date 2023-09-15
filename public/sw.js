ed4059f956850a7465fcd1b4fd089788802dee70
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-15 19:19:32 +0530

    add code swanee

--- public/sw.js
+++ public/sw.js
@@ -82,8 +82,7 @@ workbox.routing.registerRoute(
     cacheName: API_CACHE_NAME,
     plugins: [
       {
-        cacheWillUpdate: async ({ response }) => {
-            alert()
+        cacheWillUpdate: async (response ) => {
             console.log(response,"HIREN")
           // Check if the response has a 'statusCode' header and its value is 200
           const headers = response.headers;

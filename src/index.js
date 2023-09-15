43f3c827912657ca3496abccf40a824333eed000
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-15 18:41:05 +0530

    add code swanee

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import "./assets/css/style.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
-import swDev from "./swDev";
 // import { PersistGate } from "redux-persist/integration/react";
 // import { persistStore } from "redux-persist";
 // let persistor = persistStore(store);
@@ -23,5 +22,3 @@ root.render(
     </BrowserRouter>
   </Provider>
 );
-
-swDev()
--- src/swDev.js
+++
@@ -1,8 +0,0 @@
-export default function swDev(params) {
-    let swUrl=`/sw.js`
-    navigator.serviceWorker.register(swUrl).then((res)=>{
-        console.log(res,"res")
-    }).catch((err)=>{
-        console.log(err)
-    })
-}

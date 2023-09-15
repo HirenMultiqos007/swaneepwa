a38c497672de5bbbf8ccde42c703019f0b97e61d
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-15 18:35:43 +0530

    add code swanee

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./assets/css/style.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
-import {sw} from '../public/sw'
+import {sw} from './sw.js'
 // import { PersistGate } from "redux-persist/integration/react";
 // import { persistStore } from "redux-persist";
 // let persistor = persistStore(store);

26b9c12ff1212ab834fe765513ae44545518e861
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-15 17:45:35 +0530

    add code swanee

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,82 @@
+// const CACHE = "pwabuilder-offline-page";
+// const API = "api-data"
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
+
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+// const BASE_URL = 'https://staging.multiqos.com:8012';
+
+// // Cache API responses using a NetworkFirst strategy
+// workbox.routing.registerRoute(
+//   new RegExp('^' + escapeRegExp(BASE_URL) + '/api/v1/user/'),
+//   new workbox.strategies.NetworkFirst({
+//     cacheName: API,
+//     plugins: [
+//       new workbox.cacheableResponse.CacheableResponsePlugin({
+//         statuses: [0, 200], // Cache responses with status 0 and 200 (you can adjust this as needed)
+//       }),
+//     ],
+//   })
+// );
+
+// // Helper function to escape special characters in a string for use in a regular expression
+// function escapeRegExp(string) {
+//   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+// }
+
+// workbox.routing.registerRoute(
+//   new RegExp('/*'),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: CACHE
+//   })
+// );
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
+
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
+
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
+
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
+
+
 const CACHE = "pwabuilder-offline-page";
-const API = "api-data"
+const API_CACHE = "api-data";
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
@@ -22,13 +96,12 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-const BASE_URL = 'https://staging.multiqos.com:8012';
+const BASE_URL = 'https://staging.multiqos.com:8012/api/v1/user'; // Updated base URL
 
-// Cache API responses using a NetworkFirst strategy
 workbox.routing.registerRoute(
-  new RegExp('^' + escapeRegExp(BASE_URL) + '/api/v1/user/'),
+  new RegExp('^' + escapeRegExp(BASE_URL) + '/'), // Match all routes under the base URL
   new workbox.strategies.NetworkFirst({
-    cacheName: API,
+    cacheName: API_CACHE,
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200], // Cache responses with status 0 and 200 (you can adjust this as needed)
@@ -37,7 +110,6 @@ workbox.routing.registerRoute(
   })
 );
 
-// Helper function to escape special characters in a string for use in a regular expression
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }

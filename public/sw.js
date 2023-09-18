2a818cc4f79d3d29f5474e040873dd714cd9a369
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-18 10:42:04 +0530

    hiren swanee pwa

--- public/sw.js
+++ public/sw.js
@@ -1,126 +1,108 @@
-// const CACHE = "pwabuilder-offline-page";
-// const API = "api-data"
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE = "pwabuilder-offline-page";
+const API = "api-data"
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = "ToDo-replace-this-name.html";
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
-// self.addEventListener("message", (event) => {
-//   if (event.data && event.data.type === "SKIP_WAITING") {
-//     self.skipWaiting();
-//   }
-// });
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
-// self.addEventListener('install', async (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE)
-//       .then((cache) => cache.add(offlineFallbackPage))
-//   );
-// });
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
 
-// if (workbox.navigationPreload.isSupported()) {
-//   workbox.navigationPreload.enable();
-// }
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-// const BASE_URL = 'https://staging.multiqos.com:8012';
+const BASE_URL = 'https://staging.multiqos.com:8012';
 
-// // Cache API responses using a NetworkFirst strategy
-// workbox.routing.registerRoute(
-//   new RegExp('^' + escapeRegExp(BASE_URL) + '/api/v1/user/'),
-//   new workbox.strategies.NetworkFirst({
-//     cacheName: API,
-//     plugins: [
-//       new workbox.cacheableResponse.CacheableResponsePlugin({
-//         statuses: [0, 200], // Cache responses with status 0 and 200 (you can adjust this as needed)
-//       }),
-//     ],
-//   })
-// );
-
-// // Helper function to escape special characters in a string for use in a regular expression
-// function escapeRegExp(string) {
-//   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-// }
-
-// workbox.routing.registerRoute(
-//   new RegExp('/*'),
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: CACHE
-//   })
-// );
-
-// self.addEventListener('fetch', (event) => {
-//   if (event.request.mode === 'navigate') {
-//     event.respondWith((async () => {
-//       try {
-//         const preloadResp = await event.preloadResponse;
-
-//         if (preloadResp) {
-//           return preloadResp;
-//         }
-
-//         const networkResp = await fetch(event.request);
-//         return networkResp;
-//       } catch (error) {
-
-//         const cache = await caches.open(CACHE);
-//         const cachedResp = await cache.match(offlineFallbackPage);
-//         return cachedResp;
-//       }
-//     })());
-//   }
-// });
-
-
-const CACHE_NAME = "my-cache"; // Change this to a unique name
-
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
-
+// Cache API responses using a NetworkFirst strategy
 workbox.routing.registerRoute(
-  new RegExp("^https://staging.multiqos.com:8012/api/v1/user/"), // Match the API route
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE_NAME,
+  new RegExp('^' + escapeRegExp(BASE_URL) + '/api/v1/user/'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: API,
     plugins: [
-      {
-        cacheWillUpdate: async ({ response }) => {
-          console.log(response, "HIREN");
-          // Check if the response has a 'statusCode' header and its value is 200
-          const headers = response.headers;
-          const statusCode = headers.get("statusCode");
-
-          if (statusCode === "200") {
-            // Cache the response
-            return response;
-          } else {
-            // Do not cache the response
-            return null;
-          }
-        },
-      },
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200], // Cache responses with status 0 and 200 (you can adjust this as needed)
+      }),
     ],
   })
 );
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
+// BACKGROUND SYNC
 
-          if (preloadResp) {
-            return preloadResp;
-          }
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResp = await cache.match("/ToDo-replace-this-name.html");
-          return cachedResp;
+const messageAboutFailPlugin = {
+  fetchDidFail: async ({ originalRequest, request, error, event, state }) => {
+    messageClient(event, 'REQUEST_FAILED');
+  },
+};
+
+
+// Instantiating and configuring plugin
+const bgSyncPlugin = new BackgroundSyncPlugin('feedbackQueue', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+
+  onSync: async ({ queue }) => {
+    // Run standard replay
+    await queue.replayRequests();
+
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) =>
+        client.postMessage({ type: 'REPLAY_COMPLETED' })
+      );
+    });
+  },
+});
+
+
+
+// Registering a route for retries
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/feedback'),
+  new NetworkFirst({
+    plugins: [bgSyncPlugin, messageAboutFailPlugin],
+  }),
+  'POST'
+);
+
+// Helper function to escape special characters in a string for use in a regular expression
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-      })()
-    );
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });

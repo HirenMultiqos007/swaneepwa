b546eb14e2a38dbd50b6ea4e8b0a4092470e7c60
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-21 13:23:59 +0530

    hiren

--- src/App.js
+++ src/App.js
@@ -90,7 +90,6 @@ function App() {
         </Toast>
      {mode === false && <div className="offline-mode"> You are Offline Mode</div> } 
       <Routing />
-      <h1> helooo </h1>
     </>
     // <div className="app">
     //   {isRehydrating ? (
